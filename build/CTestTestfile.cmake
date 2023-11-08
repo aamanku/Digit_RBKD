6e23f6c55b52329b38176f30b72ec75f798ef81b
Abhijeet Kulkarni <amkulk@udel.edu>
2023-11-07 23:36:43 -0500

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abhijee/Documents/GitHub/Digit_RBKD
-# Build directory: /home/abhijee/Documents/GitHub/Digit_RBKD/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
